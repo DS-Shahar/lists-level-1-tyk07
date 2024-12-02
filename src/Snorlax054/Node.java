6a29519ea4374a4eaa5c92b18baf0720fda8f70e
tyk07 <swampert054@gmail.com>
2024-12-02 14:29:15 +0200

    Add files via upload

---
+++ src/Snorlax054/Node.java
@@ -0,0 +1,39 @@
+package Snorlax054;
+
+public class Node<Interger> {
+	int data;   
+    Node<Interger> next;
+    
+    public Node(int data) 
+    {
+        this.data = data;
+        this.next = null; // Default value
+    }
+    public int getData() 
+	{
+		return data;
+	}
+	public void setData(int data) 
+	{
+		this.data = data;
+	}
+	public Node<Interger> getNext() 
+	{
+		return next;
+	}
+    public void setNext(Node<Interger> next)
+    {
+    	this.next = next;
+    }
+    public String toString() 
+    {
+        StringBuilder sb = new StringBuilder();
+        Node<Interger> current = this;
+        while (current != null) {
+            sb.append(current.data).append(" -> ");
+            current = current.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
+}
